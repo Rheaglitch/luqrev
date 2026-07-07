9f40d395d363b246425ed18c0b31f97fdb434bb1
Lia <ohmyliinnn188@gmail.com>
2026-07-07 09:40:35 +0700

    fix: update admin password di zhaorukou route

--- src/app/zhaorukou/route.ts
+++ src/app/zhaorukou/route.ts
@@ -10,7 +10,7 @@ export async function GET(_req: NextRequest) {
 
   const { error } = await supabase.auth.signInWithPassword({
     email: 'ohmyliinnn@gmail.com',
-    password: 'zhaorukou4terus',
+    password: 'zhaorukou4',
   })
 
   if (error) {
